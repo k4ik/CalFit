31f23d0b5c9144bd0e048d7ae4dff07f03de08dd
k4ik <kaikleal1234@gmail.com>
2024-09-23 14:55:59 -0300

    fix: logo

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,5 +1,5 @@
 <x-app-layout>
-    <section class="flex justify-center items-center gap-4 mt-4 h-max text-black dark:text-white">
+    <section class="flex justify-center items-center gap-4 mt-4 text-black dark:text-white">
         <article class="text-center bg-gray-100 dark:bg-gray-800 p-6 rounded-lg shadow-lg transition-transform transform hover:scale-105">
             <a href="{{ route('get.index') }}" class="no-underline text-black dark:text-white hover:text-blue-500 dark:hover:text-blue-300">
                 <h2 class="text-2xl font-bold mb-2">GET</h2>
