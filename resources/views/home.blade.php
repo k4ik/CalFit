813340df3f99af661abcd9349f28c8edbb7bbca8
k4ik <kaikleal1234@gmail.com>
2024-09-24 15:56:23 -0300

    fix: bg-color

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,15 +1,15 @@
 <x-app-layout>
-    <section class="flex justify-center items-center gap-4 mt-4 text-black dark:text-white">
-        <article class="text-center bg-gray-100 dark:bg-gray-800 p-6 rounded-lg shadow-lg transition-transform transform hover:scale-105">
-            <a href="{{ route('get.index') }}" class="no-underline text-black dark:text-white hover:text-blue-500 dark:hover:text-blue-300">
-                <h2 class="text-2xl font-bold mb-2">GET</h2>
-                <p>Calcule seu Gasto Energético Total</p>
-            </a>
-        </article>
+    <section class="flex flex-wrap justify-center items-center gap-4 min-h-screen text-black dark:text-white bg-gray-300 dark:bg-gray-900">
         <article class="text-center bg-gray-100 dark:bg-gray-800 p-6 rounded-lg shadow-lg transition-transform transform hover:scale-105">
             <a href="{{ route('imc.index') }}" class="no-underline text-black dark:text-white hover:text-blue-500 dark:hover:text-blue-300">
                 <h2 class="text-2xl font-bold mb-2">IMC</h2>
                 <p>Calcule seu Índice de Massa Corporal</p>
+            </a>
+        </article>
+        <article class="text-center bg-gray-100 dark:bg-gray-800 p-6 rounded-lg shadow-lg transition-transform transform hover:scale-105">
+            <a href="{{ route('get.index') }}" class="no-underline text-black dark:text-white hover:text-blue-500 dark:hover:text-blue-300">
+                <h2 class="text-2xl font-bold mb-2">GET</h2>
+                <p>Calcule seu Gasto Energético Total</p>
             </a>
         </article>
         <article class="text-center bg-gray-100 dark:bg-gray-800 p-6 rounded-lg shadow-lg transition-transform transform hover:scale-105">
